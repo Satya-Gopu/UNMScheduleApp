09fceef6456fd7eb04af76345dfa80f4c99de712
satya <sgopu@unm.edu>
2017-06-15 22:24:07 -0600

    Make sure running perfect

--- UNMScheduleApp/ViewController7.swift
+++ UNMScheduleApp/ViewController7.swift
@@ -23,6 +23,17 @@ class ViewController7: UIViewController {
     @IBOutlet weak var timing: UILabel!
     @IBOutlet weak var register: UIButton!
     @IBOutlet weak var days: UILabel!
+    @IBOutlet weak var crnHeaderLabel: UILabel!
+    @IBOutlet weak var maxEnrollHeaderLabel: UILabel!
+    @IBOutlet weak var currentEnrollHeaderLabel: UILabel!
+    @IBOutlet weak var instructionalHeaderLabel: UILabel!
+    @IBOutlet weak var daysHeaderLabel: UILabel!
+    @IBOutlet weak var emailHeaderLabel: UILabel!
+    @IBOutlet weak var instructorHeaderLabel: UILabel!
+    @IBOutlet weak var timeHeaderLabel: UILabel!
+    @IBOutlet weak var creditsHeaderLabel: UILabel!
+    @IBOutlet weak var partOfTermLabel: UILabel!
+    
     
     var titles : String = String()
     var crn : String = String()
@@ -36,10 +47,48 @@ class ViewController7: UIViewController {
     
 
     override func viewDidLoad() {
+        crnlabel.layer.borderColor = UIColor.black.cgColor
+        crnlabel.layer.borderWidth = 3
+        instructionaltype.layer.borderColor = UIColor.black.cgColor
+        instructionaltype.layer.borderWidth = 3
+        partofterm.layer.borderColor = UIColor.black.cgColor
+        partofterm.layer.borderWidth = 3
+        maxenrolllabel.layer.borderColor = UIColor.black.cgColor
+        maxenrolllabel.layer.borderWidth = 3
+        nowenrolllabel.layer.borderColor = UIColor.black.cgColor
+        nowenrolllabel.layer.borderWidth = 3
+        creditslabel.layer.borderColor = UIColor.black.cgColor
+        creditslabel.layer.borderWidth = 3
+        instructorlabel.layer.borderColor = UIColor.black.cgColor
+        instructorlabel.layer.borderWidth = 3
+        emaillabel.layer.borderColor = UIColor.black.cgColor
+        emaillabel.layer.borderWidth = 3
+        timing.layer.borderColor = UIColor.black.cgColor
+        timing.layer.borderWidth = 3
+        days.layer.borderColor = UIColor.black.cgColor
+        days.layer.borderWidth = 3
+        crnHeaderLabel.layer.borderColor = UIColor.black.cgColor
+        crnHeaderLabel.layer.borderWidth = 3
+        maxEnrollHeaderLabel.layer.borderColor = UIColor.black.cgColor
+        maxEnrollHeaderLabel.layer.borderWidth = 3
+        currentEnrollHeaderLabel.layer.borderColor = UIColor.black.cgColor
+        currentEnrollHeaderLabel.layer.borderWidth = 3
+        instructorHeaderLabel.layer.borderColor = UIColor.black.cgColor
+        instructorHeaderLabel.layer.borderWidth = 3
+        daysHeaderLabel.layer.borderColor = UIColor.black.cgColor
+        daysHeaderLabel.layer.borderWidth = 3
+        emailHeaderLabel.layer.borderColor = UIColor.black.cgColor
+        emailHeaderLabel.layer.borderWidth = 3
+        instructionalHeaderLabel.layer.borderColor = UIColor.black.cgColor
+        instructionalHeaderLabel.layer.borderWidth = 3
+        timeHeaderLabel.layer.borderColor = UIColor.black.cgColor
+        timeHeaderLabel.layer.borderWidth = 3
+        creditsHeaderLabel.layer.borderColor = UIColor.black.cgColor
+        creditsHeaderLabel.layer.borderWidth = 3
+        partOfTermLabel.layer.borderColor = UIColor.black.cgColor
+        partOfTermLabel.layer.borderWidth = 3
         super.viewDidLoad()
         self.register.layer.cornerRadius = 3.0
-        self.register.setTitleColor(UIColor.red, for: .selected)
-        //self.register.setBackgroundImage(UIImage(UIColor.black), for: .selected)
         self.crnlabel.text = self.section.crn == "" ? "not availabale" : self.section.crn
         self.coursetitle.text = self.section.title  == "" ? "not available" : self.section.title
         self.partofterm.text = self.section.partOfTerm == "" ? "not available" : self.section.partOfTerm
@@ -66,12 +115,14 @@ class ViewController7: UIViewController {
             self.days.text = "not available"
             self.timing.text = "not available"
         }
-        
         self.automaticallyAdjustsScrollViewInsets = false
+        let rightItem = UIBarButtonItem(title: "StartOver", style: .plain, target: self, action: #selector(popToRoot))
+        self.navigationItem.setRightBarButton(rightItem, animated: true)
         
-        self.navigationItem.rightBarButtonItem?.title = "StartOver"
-        //self.navigationItem.rightBarButtonItem.
-        
+    }
+    
+    func popToRoot(){
+        self.navigationController?.popToRootViewController(animated: true)
     }
     
     override func viewWillAppear(_ animated: Bool) {
